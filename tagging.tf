2842fcaaefc9a98bf7de74fed688ef23c90b7e62
PK <pknw1@users.noreply.github.com>
2023-05-09 03:59:54 +0100

    init

---
+++ tagging.tf
@@ -0,0 +1,36 @@
+locals {
+  is_powershell = var.interpreter == "PowerShell"
+  trimmed_tags = {
+    for key, value in var.tags :
+    key => value if value != null
+  }
+  tags = join(" ", formatlist("%s=\"%s\"", keys(local.trimmed_tags), values(local.trimmed_tags)))
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "null_resource" "tags" {
+
+  count = var.nb_resources
+
+  triggers = {
+    resource    = var.resource_ids[count.index]
+    tags        = jsonencode(var.tags)
+    force-apply = var.force ? uuid() : false
+    behavior    = var.behavior
+  }
+
+  # Code found here https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-group-using-tags#code-try-22
+  provisioner "local-exec" {
+    interpreter = local.is_powershell ? ["PowerShell"] : ["/bin/bash", "-c"]
+    command = templatefile(format("%s/script/%s", path.module, local.is_powershell ? "tag.ps1.tmpl" : "tag.sh.tmpl"),
+      {
+        behavior        = var.behavior
+        resource_id     = var.resource_ids[count.index]
+        tags            = local.tags
+        subscription_id = data.azurerm_client_config.current.subscription_id
+      }
+    )
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "resource_ids" {
+  description = "Id of the Azure resources to tag"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Tags to apply on resource"
+  type        = map(string)
+}
+
+variable "force" {
+  description = "True to apply every time"
+  type        = bool
+  default     = false
+}
+
+variable "behavior" {
+  description = "Behavior for tags applying. Must be `merge` or `overwrite`."
+  type        = string
+  default     = "merge"
+  validation {
+    condition     = contains(["merge", "overwrite"], var.behavior)
+    error_message = "The value for behavior must be either `merge` or `overwrite`."
+  }
+}
+
+variable "nb_resources" {
+  description = "Number of resources to tag"
+  type        = number
+}
+
+variable "interpreter" {
+  description = "System interpreter to use for tagging script."
+  type        = string
+  default     = "bash"
+  validation {
+    condition     = contains(["bash", "PowerShell"], var.interpreter)
+    error_message = "The value for interpreter must be either `bash` or `PowerShell`."
+  }
+}
